270d8d8e9e4feaabe9e5c26ff5c432c34b88cdf7
Serhii Zakutko <serhii.zakutko@nixsolutions.com>
2023-01-18 15:12:15 +0100

    18.01.2023 Started implementation client app on angular

---
+++ src/app/advertisements/advertisements.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AdvertisementsComponent } from './advertisements.component';
+
+describe('AdvertisementsComponent', () => {
+  let component: AdvertisementsComponent;
+  let fixture: ComponentFixture<AdvertisementsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AdvertisementsComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AdvertisementsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AdvertisementsComponent } from './advertisements/advertisements.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    AdvertisementsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      {
+        path: "",
+        component: HomeComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
